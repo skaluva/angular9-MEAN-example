b9a158997e00a7bfec19a1f025ff67900656acf0
skaluva <sreenivasulu.kaluva84@gmail.com>
2020-05-14 13:57:06 +0530

    changes

--- sales-report-client/src/app/app.module.ts
+++ sales-report-client/src/app/app.module.ts
@@ -11,6 +11,19 @@ import { EditSalesComponent } from './edit-sales/edit-sales.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
@@ -26,7 +39,19 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ChartsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
